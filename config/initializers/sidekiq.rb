9dbe95733ebb8710b1982670d218aa4841061e32
Arthur Neves <arthurnn@gmail.com>
2015-05-23 01:35:11 +0200

    Initial commit

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,9 @@
+Rails.application.config.queue_adapter = :sidekiq
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: Shipit.redis_url.to_s }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: Shipit.redis_url.to_s }
+end
